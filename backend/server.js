e50c1ebbfd069465f393551a3310b4b7b49da990
nagababu112904 <nagababu112904@gmail.com>
2025-12-23 21:27:37 +0530

    Update server.js
    fix cors

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
+import cors from 'cors';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -16,16 +16,16 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(
   cors({
     origin: 'https://primepickz-frontend.onrender.com',
     credentials: true,
   })
-)
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
