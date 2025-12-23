f902d1da8e76b903a8af859543eed54fbef7b85c
nagababu112904 <nagababu112904@gmail.com>
2025-12-23 20:53:23 +0530

    Update constants.js
    Fix frontend API base URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,8 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+export const BASE_URL = 'https://primepickz-ecommerce.onrender.com';
+
+export const PRODUCTS_URL = ⁠ ${BASE_URL}/api/products ⁠;
+export const USERS_URL = ⁠ ${BASE_URL}/api/users ⁠;
+export const ORDERS_URL = ⁠ ${BASE_URL}/api/orders ⁠;
+export const PAYPAL_URL = ⁠ ${BASE_URL}/api/config/paypal ⁠;
